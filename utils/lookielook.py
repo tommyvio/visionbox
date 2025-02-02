0587c0a8bdf4fa70cd6c008056f4d4dac5df3f57
Tommy 汤米 <48564227+tommyvio@users.noreply.github.com>
2025-02-01 22:04:42 -0500

    Create lookielook.py

---
+++ utils/lookielook.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plot_training_history(history):
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower right')
+    plt.show()
